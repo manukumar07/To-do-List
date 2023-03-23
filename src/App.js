f039837ae70845ce5a169bcb211d75bd455a2b75
Manu Kumar Pal <manukumarpal34@gmail.com>
2023-03-24 00:35:23 +0530

    add components folder and Todojs files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Todo from './components/Todo';
+import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-    
+    <Todo />
     </div>
   );
 }
